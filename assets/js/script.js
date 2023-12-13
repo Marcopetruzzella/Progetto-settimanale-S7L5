23eee8ef7030c9f3e250f40a6e081893ccb7030e
Marco091 <Marco91fire@gmail.com>
2023-12-13 16:27:55 +0100

    edir 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 let autkey= "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4NWY0YjI2NzYxNDAwMTgzYzJlY2UiLCJpYXQiOjE3MDIzODc1MzIsImV4cCI6MTcwMzU5NzEzMn0.M9RbWAXZ1p8PJMLRd8DUMik8rIh6Hx_Uod6BwsW2gQo"
-let dataToSend = {
-    
-    nome: 'Marco'};
+
 
   document.addEventListener('DOMContentLoaded', () => {
-    let obj = {
+   /*  let obj = {
         name: "Mario Rossi",
         description : "Mariolino",
         brand: "motorola",
         imageUrl: "http://mariorossi.com",
         price: 25
     }
-        console.log(JSON.stringify(obj))
+        console.log(JSON.stringify(obj)) */
     
         function scrivoApi (obj) {fetch("https://striveschool-api.herokuapp.com/api/product", {
             // Chiamata di tipo POST
@@ -31,9 +29,30 @@ let dataToSend = {
         .then(response => response.json())
         .then(json => {
             console.log(json)
-            //createPhotosCard(json.photos);
-        })
-        .catch(error => console.log(error))
+            let prodlist = document.querySelector('.prod-list')
+            
+            prodlist.textContent="";
+            console.log(prodlist)
+            json.forEach(function(numero, indice) {
+                let div = document.createElement('div')
+                div.classList = 'd-flex w-auto'
+                div.innerHTML = ` <div class="card m-1 col-3 p-3" style="width: 18rem; " >
+                <img src="${json[indice].imageUrl}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${json[indice].name}</h5>
+                 
+                  <p class="card-text">${json[indice].description}.</p>
+                  <p class="card-text">Prezzo: <span class="price">${json[indice].price}</span></p>
+                  <p class="card-text">Brand: <span class="brand">${json[indice].brand}</span></p>
+                  <a href="#" class="btn btn-primary">Acquista</a>
+                  <h5 class="idscheda"">id=${json[indice]._id}</h5>
+                </div>
+              </div>`
+                prodlist.appendChild(div)
+                });
+              });
+           
+        
     }
   /*   leggopai()
     scrivoApi()
@@ -58,10 +77,41 @@ let dataToSend = {
         scrivoApi (oggettoDati)
         leggopai()
       }
-    
-       document.querySelector('.btn-primary').addEventListener('click', () => { creaOggetto ()})
-      
-      
+      function rimuoviogg() {
+        let iddel = document.querySelector('#idprod').value; // Assicurati di usare '#idprod' se è un ID o '.idprod' se è una classe
+        console.log(iddel);
+        
+        fetch(`https://striveschool-api.herokuapp.com/api/product/${iddel}`, { 
+            method: "DELETE", 
+            headers: {
+                'Authorization': autkey, 
+                "Content-Type": "application/json"
+            } 
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Errore nella richiesta di eliminazione');
+            }
+            return response.json(); 
+        })
+        .then(data => {
+            console.log('Elemento eliminato con successo', data);
+            
+        })
+        .catch(error => {
+            console.error('Si è verificato un errore durante la cancellazione:', error);
+            
+        });
+    }
 
+
+
+
+        
+       document.querySelector('.btn-primary').addEventListener('click', () => { creaOggetto ()})
+       leggopai()
+
+       document.querySelector('.btn-danger').addEventListener('click', () =>  { rimuoviogg()})
+       
 })
 
