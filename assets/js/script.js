43dea52f93e5336c5b2b0b15d921914fb1ea65ae
Marco091 <Marco91fire@gmail.com>
2024-01-03 00:55:38 +0100

    edit 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,117 +1,183 @@
-let autkey= "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4NWY0YjI2NzYxNDAwMTgzYzJlY2UiLCJpYXQiOjE3MDIzODc1MzIsImV4cCI6MTcwMzU5NzEzMn0.M9RbWAXZ1p8PJMLRd8DUMik8rIh6Hx_Uod6BwsW2gQo"
+let autkey = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4NWY0YjI2NzYxNDAwMTgzYzJlY2UiLCJpYXQiOjE3MDM4ODI2NzAsImV4cCI6MTcwNTA5MjI3MH0.jSQpncHxbr8wO8FhCgN7XVtYWDQI1JPLl3tt9Lcs1lI"
 
 
-  document.addEventListener('DOMContentLoaded', () => {
-   /*  let obj = {
-        name: "Mario Rossi",
-        description : "Mariolino",
-        brand: "motorola",
-        imageUrl: "http://mariorossi.com",
-        price: 25
-    }
-        console.log(JSON.stringify(obj)) */
-    
-        function scrivoApi (obj) {fetch("https://striveschool-api.herokuapp.com/api/product", {
-            // Chiamata di tipo POST
-            method: "POST", // Method della chiamata - Salvataggio di una risorsa
-            body: JSON.stringify(obj), // nel body della richiesta invio il dato al server
-            headers: {
-                'Authorization' : autkey, "Content-Type": "application/json",  // il tipo del contenuto che sto inviando
-            },
-          })}
-    
-    function leggopai( ) {
-        let url = `https://striveschool-api.herokuapp.com/api/product`;
-        fetch(url, {
-            method: 'GET', 
-            headers: { 'Authorization': autkey }
-        })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json)
-            let prodlist = document.querySelector('.prod-list')
-            
-            prodlist.textContent="";
-            console.log(prodlist)
-            json.forEach(function(numero, indice) {
-                let div = document.createElement('div')
-                div.classList = 'd-flex w-auto'
-                div.innerHTML = ` <div class="card m-1 col-3 p-3" style="width: 18rem; " >
-                <img src="${json[indice].imageUrl}" class="card-img-top" alt="...">
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  function scrivoApi(obj) {
+    fetch("https://striveschool-api.herokuapp.com/api/product", {
+
+      method: "POST",
+      body: JSON.stringify(obj),
+      headers: {
+        'Authorization': autkey, "Content-Type": "application/json",
+      },
+    })
+  }
+
+  function leggopai() {
+    let url = `https://striveschool-api.herokuapp.com/api/product`;
+    fetch(url, {
+      method: 'GET',
+      headers: { 'Authorization': autkey }
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        let prodlist = document.querySelector('.prod-list')
+        prodlist.textContent = ""
+
+
+
+
+
+        json.forEach(json => {
+          console.log(json)
+          let div = document.createElement('div')
+          div.classList = "w-25"
+          div.innerHTML = ` 
+                
+                <div class="card m-1 col-3 " style="width: 18rem; " >
+                <img src="${json.imageUrl}"  class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">${json[indice].name}</h5>
+                  <h5 class="card-title">${json.name}</h5>
                  
-                  <p class="card-text">${json[indice].description}.</p>
-                  <p class="card-text">Prezzo: <span class="price">${json[indice].price}</span></p>
-                  <p class="card-text">Brand: <span class="brand">${json[indice].brand}</span></p>
+                  <p class="card-text">Prezzo: <span class="price">${json.price} Euro</span></p>
+                  <p class="card-text">Brand: <span class="brand">${json.brand}</span></p>
                   <a href="#" class="btn btn-primary">Acquista</a>
-                  <h5 class="idscheda"">id=${json[indice]._id}</h5>
+                  <button type="button" class="btn btn-primary btninfo" id="${json._id}" data-bs-toggle="modal" data-bs-target="#exampleModal">Maggiori info</button>
                 </div>
-              </div>`
-                prodlist.appendChild(div)
-                });
-              });
-           
-        
-    }
-  /*   leggopai()
-    scrivoApi()
-    leggopai() */
-    
-    function creaOggetto() {
-        let name = document.getElementById('name').value;
-        let description = document.getElementById('description').value;
-        let brand = document.getElementById('brand').value;
-        let imageUrl = document.getElementById('imageUrl').value;
-        let price = document.getElementById('price').value;
-    
-        let oggettoDati = {
-          name: name,
-          description: description,
-          brand: brand,
-          imageUrl: imageUrl,
-          price: price
-        };
-    
-        console.log(oggettoDati); 
-        scrivoApi (oggettoDati)
-        leggopai()
-      }
-      function rimuoviogg() {
-        let iddel = document.querySelector('#idprod').value; // Assicurati di usare '#idprod' se è un ID o '.idprod' se è una classe
-        console.log(iddel);
-        
-        fetch(`https://striveschool-api.herokuapp.com/api/product/${iddel}`, { 
-            method: "DELETE", 
-            headers: {
-                'Authorization': autkey, 
-                "Content-Type": "application/json"
-            } 
-        })
+              </div></div>
+              `
+
+
+
+
+
+
+
+
+          prodlist.appendChild(div)
+
+        });
+      });
+
+
+  }
+
+
+  function creaOggetto() {
+    let name = document.getElementById('name').value;
+    let description = document.getElementById('description').value;
+    let brand = document.getElementById('brand').value;
+    let imageUrl = document.getElementById('imageUrl').value;
+    let price = document.getElementById('price').value;
+
+    let oggettoDati = {
+      name: name,
+      description: description,
+      brand: brand,
+      imageUrl: imageUrl,
+      price: price
+    };
+
+    console.log(oggettoDati);
+    scrivoApi(oggettoDati)
+    leggopai()
+  }
+
+
+
+
+
+
+  document.querySelector('.btn-primary').addEventListener('click', () => { creaOggetto() })
+  leggopai()
+
+  document.querySelector('.btn-danger').addEventListener('click', () => { rimuoviogg() })
+
+})
+
+
+
+function confirmReset() {
+  if (confirm("Sei sicuro di voler resettare il modulo?")) {
+    document.getElementById("myForm").reset();
+  }
+  else { }
+}
+function confirmdelete() {
+  if (confirm("Sei sicuro di vole cancellare il prodotto dal database?")) {
+    {
+      let iddel = document.querySelector('#idprod').value;
+      console.log(iddel);
+
+      fetch(`https://striveschool-api.herokuapp.com/api/product/${iddel}`, {
+        method: "DELETE",
+        headers: {
+          'Authorization': autkey,
+          "Content-Type": "application/json"
+        }
+      })
         .then(response => {
-            if (!response.ok) {
-                throw new Error('Errore nella richiesta di eliminazione');
-            }
-            return response.json(); 
+          if (!response.ok) {
+            throw new Error('Errore nella richiesta di eliminazione');
+          }
+          return response.json();
         })
         .then(data => {
-            console.log('Elemento eliminato con successo', data);
-            
+          console.log('Elemento eliminato con successo', data);
+
         })
         .catch(error => {
-            console.error('Si è verificato un errore durante la cancellazione:', error);
-            
+          console.error('Si è verificato un errore durante la cancellazione:', error);
+
         });
     }
 
+  }
+  else { }
+}
 
+if (window.location.pathname === "/backoffice.html") {
+  document.querySelector("form#myForm button.btn-secondary").addEventListener("click", () => { confirmReset() });
+  document.querySelector("form#deletorm button ").addEventListener("click", () => { confirmdelete() });
+}
 
-
-        
-       document.querySelector('.btn-primary').addEventListener('click', () => { creaOggetto ()})
-       leggopai()
-
-       document.querySelector('.btn-danger').addEventListener('click', () =>  { rimuoviogg()})
-       
-})
+document.querySelector(".prod-list").addEventListener("click", (e) => {
+  if (e.target.innerHTML === "Maggiori info") {
+    console.log(e.target.id)
+    let url = `https://striveschool-api.herokuapp.com/api/product/${e.target.id}`;
+    fetch(url, {
+      method: 'GET',
+      headers: { 'Authorization': autkey }
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        let modal = document.querySelector("#exampleModal");
+        modal.innerHTML = `   
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel"> ${json.name}</h1>
+                
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+              <div class="w-100"><img src="${json.imageUrl}" width="50%"></div>
+              <p class="card-text">Prezzo: <span class="price">${json.price} Euro</span></p>
+                  <p class="card-text">Brand: <span class="brand">${json.brand}</span></p>
+                  
+              <p class="card-text">${json.description}</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                <button type="button" class="btn btn-primary">Aggiungi al carrello</button>
+              </div>
+            </div>
+          </div>`;
+      });
+  }
+});
 
